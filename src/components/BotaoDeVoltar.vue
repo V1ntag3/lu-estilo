570c7925c5db7d9229867d9a55977e680e9eb323
V1ntag3 <84781701+V1ntag3@users.noreply.github.com>
2022-09-25 19:48:24 -0300

    Componente Botão  de voltar da Descrição do Produto

---
+++ src/components/BotaoDeVoltar.vue
@@ -0,0 +1,22 @@
+<template>
+    <button id="voltar">
+        <img src="../assets/setadevoltar.png" alt="">
+    </button>
+</template>
+
+<script>
+
+</script>
+<style>
+#voltar {
+    transition: 0.5s;
+    background-color: white;
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    border: none;
+    left: 10px;
+    top: 20px;
+    border-radius: 50%;
+}
+</style>
